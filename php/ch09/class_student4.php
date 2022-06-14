4e39c4906ac7e2c355a46fc5b75003a37b56953a
hwi0523 <miney6563@gmail.com>
2022-06-14 18:09:46 +0900

    php

--- php/ch09/class_student4.php
+++ php/ch09/class_student4.php
@@ -27,13 +27,7 @@
         }
     }
 
-    $obj = new Student();
-    //print $obj->setStudentId(1122)->getStudentId() . "<br>";      
-    print "1번째 : " . $obj->setStudentId(1122)->getStudentId() . "<br>";
-
-    $obj2 = new Student;
-    print $obj2->setStudentId(8888)->getStudentId() . "<br>";  
-    print "2번째 : " . $obj->getStudentId() . "<br>";
-
-   // $obj->printStudent();
-
+    $obj = new Student;
+    $obj -> setStudentId(1122);
+    $obj -> setStudentName("Alice");
+    $obj -> printStudent();
